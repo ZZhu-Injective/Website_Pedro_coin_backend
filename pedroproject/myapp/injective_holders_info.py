93b99b3de50acf5f78633cca46fec6e9d9cf066a
Zonwie <102626057+Zonwiezhu@users.noreply.github.com>
2025-01-13 17:24:49 +0100

    Latest update

--- pedroproject/myapp/injective_holders_info.py
+++ pedroproject/myapp/injective_holders_info.py
@@ -1,4 +1,3 @@
-import asyncio
 import pandas as pd
 import base64
 import json
@@ -15,6 +14,8 @@ class InjectiveHolders:
         self.network = Network.mainnet()
         self.client = AsyncClient(self.network)
 
+
+    # Fetch holders from CW20 Token
     async def fetch_holders_cw20(self, cw20_address):
         retry_attempts = 5
         holders = None
@@ -26,13 +27,11 @@ class InjectiveHolders:
             except aio.AioRpcError as e:
                 if e.code() == StatusCode.UNAVAILABLE and attempt < retry_attempts - 1:
                     wait_time = 2 ** attempt
-                    print(f"Retrying in {wait_time} seconds...")
                     time.sleep(wait_time)
                 else:
                     raise
 
         if holders is None:
-            print("Failed to fetch holders after multiple attempts")
             return
 
         A = holders
@@ -64,13 +63,13 @@ class InjectiveHolders:
                 continue
 
         df_holders_cw20 = pd.DataFrame(data_wallet)
-        wallet_count = df_holders_cw20.shape[0]
-        print(df_holders_cw20)
-        print(f"Total number of wallets: {wallet_count}")
 
         return df_holders_cw20
 
 
+
+
+    # Fetch holders from native Token
     async def fetch_holder_native_token(self, native_address):
         retry_attempts = 5
         holders = None
@@ -82,13 +81,11 @@ class InjectiveHolders:
             except aio.AioRpcError as e:
                 if e.code() == StatusCode.UNAVAILABLE and attempt < retry_attempts - 1:
                     wait_time = 2 ** attempt
-                    print(f"Retrying in {wait_time} seconds...")
                     time.sleep(wait_time)
                 else:
                     raise
 
         if holders is None:
-            print("Failed to fetch holders after multiple attempts")
             return
 
         B = holders
@@ -105,12 +102,24 @@ class InjectiveHolders:
                 amount_Coin = model['balance']['amount']
                 inj_address = model['address']
 
-                fetch_info = await self.client.fetch_denom_metadata(denom=native_address)
-
-                if fetch_info['metadata']['decimals'] == 0:
-                    amount_Coin = int(amount_Coin) / 10 ** 18
+                if native_address == "factory/inj127l5a2wmkyvucxdlupqyac3y0v6wqfhq03ka64/qunt":
+                    decimal = 6
+                elif native_address == "factory/inj1xy3kvlr4q4wdd6lrelsrw2fk2ged0any44hhwq/KIRA":
+                    decimal = 6
+                elif native_address == "factory/inj1cw3733laj4zj3ep5ndx2sfz0aed0u03kwt6ucc/ffi":
+                    decimal = 6
+                elif native_address == "factory/inj178zy7myyxewek7ka7v9hru8ycpvfnen6xeps89/DRUGS":
+                    decimal = 6
+                elif native_address == "factory/inj10aa0h5s0xwzv95a8pjhwluxcm5feeqygdk3lkm/SAI":
+                    decimal = 18
                 else:
-                    amount_Coin = int(amount_Coin) / 10 ** fetch_info['metadata']['decimals']
+                    fetch_info = await self.client.fetch_denom_metadata(denom=native_address)
+                    if fetch_info['metadata']['decimals'] == 0:
+                        decimal = 18
+                    else:
+                        decimal = fetch_info['metadata']['decimals']
+
+                amount_Coin = int(amount_Coin) / 10 ** decimal
 
                 if amount_Coin == 0:
                     continue
@@ -121,51 +130,57 @@ class InjectiveHolders:
                 continue
 
         df_holders_native_coins = pd.DataFrame(data_wallet)
-        wallet_count = df_holders_native_coins.shape[0]
-        print(df_holders_native_coins)
-        print(f"Total number of wallets: {wallet_count}")
-
         return df_holders_native_coins
+    
 
+
+
+
+    # Merge this two data to one dataframe.
     async def fetch_holders(self, cw20_address, native_address):
-        print(cw20_address)
 
-        df_holders_native =  await self.fetch_holder_native_token(native_address)
-        df_holders_cw20 = await self.fetch_holders_cw20(cw20_address)
+        if cw20_address == "no_cw20":
+            df_holders_native = await self.fetch_holder_native_token(native_address)
 
-        # Rename the columns to distinguish between native and CW20 values
-        df_holders_cw20.rename(columns={'value': 'cw20_value'}, inplace=True)
-        df_holders_native.rename(columns={'value': 'native_value'}, inplace=True)
+            df_holders_native.rename(columns={'value': 'native_value'}, inplace=True)
+            print(df_holders_native)
 
-        # Merge the two dataframes on the 'key' column
+            # Creating a dummy dataframe for cw20 with the same keys and default values
+            df_holders_cw20 = df_holders_native.copy()
+            df_holders_cw20['cw20_value'] = 0
+            df_holders_cw20 = df_holders_cw20.drop(columns=['native_value'])
+            print(df_holders_cw20)
+
+        elif native_address == "no_native":
+            df_holders_cw20 = await self.fetch_holders_cw20(cw20_address)
+
+        else:
+            df_holders_native = await self.fetch_holder_native_token(native_address)
+            df_holders_cw20 = await self.fetch_holders_cw20(cw20_address)
+
+            df_holders_cw20.rename(columns={'value': 'cw20_value'}, inplace=True)
+            df_holders_native.rename(columns={'value': 'native_value'}, inplace=True)
+
+        # Merging dataframes
         merged_df = pd.merge(df_holders_native, df_holders_cw20, on='key', how='outer')
 
-        # Fill NaN values with 0
         merged_df.fillna(0, inplace=True)
 
-        # Calculate the total value
         merged_df['total_value'] = merged_df['native_value'] + merged_df['cw20_value']
 
-        # Calculate the percentage of each holder's total value
         total_supply = merged_df['total_value'].sum()
         merged_df['percentage'] = (merged_df['total_value'] / total_supply) * 100
 
-        # Sort the merged dataframe by 'total_value' from high to low
         merged_df = merged_df.sort_values(by='total_value', ascending=False)
 
-        # Round the values to two decimal places
-        merged_df = merged_df.round({'total_value': 2, 'percentage': 2, 'native_value': 2, 'cw20_value': 2})
+        merged_df = merged_df.round({'total_value': 0, 'percentage': 5, 'native_value': 2, 'cw20_value': 2})
 
-        # Add an ID column for the top holders starting from 1
         merged_df = merged_df.reset_index(drop=True)
         merged_df['Top'] = merged_df.index + 1
 
-        # Print the merged dataframe
-        print(merged_df)
-
-        # Create a dictionary from the merged dataframe
         dict_holders = merged_df.to_dict('records')
 
         print(dict_holders)
 
         return dict_holders
+    
